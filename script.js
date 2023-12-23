9c449052d44fe750da1acdcd145aa6c594086eef
ved4a <vedaas.anand@gmail.com>
2023-12-23 19:18:32 +0530

    feat: Attempt to create a 16x16 grid

---
+++ script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const mainGrid = document.querySelector("#main-grid");
+
+function create16x16() {
+  for (let i = 0; i < 16; i++) {
+    const row = document.createElement("div");
+    row.classList.add("grid-row");
+
+    for (let j = 0; j < 16; i++) {
+      const div = document.createElement("div");
+      div.classList.add("grid-item");
+      row.appendChild(div);
+    }
+    mainGrid.appendChild(row);
+  }
+}
+
+create16x16();
